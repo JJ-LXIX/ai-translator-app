e84c4f4ada9ad8b9d56b2b6027912d2810c1c2d9
jagrathshetty <jagrathshettylxix@gmail.com>
2024-11-29 17:13:49 +0530

    created getTranslation function

--- mongodb/models/User.ts
+++ mongodb/models/User.ts
@@ -1,8 +1,10 @@
 import mongoose, { Document, Schema } from "mongoose";
 import connectDB from "../db";
 import { timeStamp } from "console";
+import { Key } from "react";
 
 export interface ITranslation extends Document {
+  _id: string;
   timestamp: Date;
   fromText: string;
   from: string;
@@ -56,14 +58,57 @@ export async function addOrUpdateUser(
       options
     );
 
-    console.log("User added or updated: ", user);
-
     if (!user) {
       throw new Error("User not found and was not created");
     }
     return user;
   } catch (error) {
     console.error("error adding or updating user:", error);
+    throw error;
+  }
+}
+
+export async function getTranslations(
+  userId: string
+): Promise<Array<ITranslation>> {
+  await connectDB();
+
+  try {
+    const user: IUser | null = await User.findOne({ userId });
+
+    if (user) {
+      user.translations.sort(
+        (a: ITranslation, b: ITranslation) =>
+          b.timestamp.getTime() - a.timestamp.getTime()
+      );
+      return user.translations;
+    } else {
+      console.log(`User with userId ${userId} not found.`);
+      return [];
+    }
+  } catch (error) {
+    console.log("Error retrieving translations: ", error);
+    throw error;
+  }
+}
+
+export async function removeTranslation(
+  userId: string,
+  translationId: string
+): Promise<IUser> {
+  await connectDB();
+  try {
+    const user: IUser | null = await User.findOneAndUpdate(
+      { userId: userId },
+      { $pull: { translations: { _id: translationId } } },
+      { new: true }
+    );
+
+    if (!user) throw new Error("User not found.");
+    console.log("Translation removed: ", user);
+    return user;
+  } catch (error) {
+    console.error("Error removing translation ", error);
     throw error;
   }
 }
