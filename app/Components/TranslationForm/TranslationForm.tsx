b82439da8e98803132f89356b99b41e5e1bd2e44
jagrathshetty <jagrathshettylxix@gmail.com>
2024-12-02 11:37:55 +0530

    added voice functionality

--- app/Components/TranslationForm/TranslationForm.tsx
+++ app/Components/TranslationForm/TranslationForm.tsx
@@ -12,7 +12,9 @@ import {
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import translate from "@/actions/translate";
-import { LetterText } from "lucide-react";
+import { LetterText, Volume2Icon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Recorder, { mimeType } from "../Recorder/Recorder";
 
 type Props = {
   languages: TranslationLanguages;
@@ -50,8 +52,41 @@ function TranslationForm({ languages }: Props) {
     };
   }, [input]);
 
+  useEffect(() => {
+    if (state.output) {
+      setOutput(state.output);
+    }
+  }, [state]);
+
+  const playAudio = async () => {
+    const synth = window.speechSynthesis;
+    console.log(synth);
+    if (!output || !synth) return;
+    const wordsToSay = new SpeechSynthesisUtterance(output);
+    synth.speak(wordsToSay);
+  };
+
+  const uploadAudio = async (blob: Blob) => {
+    const file = new File([blob], mimeType, { type: mimeType });
+
+    const formData = new FormData();
+    formData.append("audio", file);
+
+    const response = await fetch("/transcribeAudio", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.text) {
+      setInput(data.text);
+    }
+  };
+
   return (
     <div>
+      <form action={formAction}>
       <div className="flex space-x-2">
         <div className="flex items-center group cursor-pointer border rounded-md w-fit px-3 py-2 bg-[#e7f0fe] mb-5">
           <LetterText />
@@ -59,6 +94,7 @@ function TranslationForm({ languages }: Props) {
             Text
           </p>
         </div>
+          <Recorder uploadAudio={uploadAudio} />
       </div>
 
       <form action={formAction}>
@@ -122,6 +158,19 @@ function TranslationForm({ languages }: Props) {
                 </SelectGroup>
               </SelectContent>
             </Select>
+              <Button
+                variant="ghost"
+                type="button"
+                onClick={playAudio}
+                className=""
+                disabled={!output}
+              >
+                <Volume2Icon
+                  size={30}
+                  className="text-blue-500 cursor-pointer disabled:cursor-not-allowed"
+                />
+              </Button>
+            </div>
             <Textarea
               placeholder="Type your message here"
               className="min-h-32 text-xl"
